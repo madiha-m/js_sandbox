7b47efd42815eeb476c5b6d0754d411ddd7fa09b
madiha-m <madiha.muhammad.dev@gmail.com>
2025-09-26 16:28:47 +0500

    feat: add navigation routes and page structure
    - Create /about and /contact route directories
    - Add basic navigation links to homepage
    - Set up foundation for multi-page application
    - Prepare project structure for future content expansion

--- alternate-skeleton-for-pic/app/page.tsx
+++ alternate-skeleton-for-pic/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      home page
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/contact">Contact</Link>
     </div>
   );
 }
